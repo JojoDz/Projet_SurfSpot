4975abbaebb4ad0289bee6bdea0f8ef4135b31e9
JojoDz <72552227+JojoDz@users.noreply.github.com>
2021-05-03 12:34:06 +0200

    image, name and place added + structure + array

---
+++ ProjetSurfSpot/ProjetSurfSpot/View/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  ProjetSurfSpot
+//
+//  Created by Joanna, Manon and Francelin on 03/05/2021.
+//
+
+import SwiftUI
+
+
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Passage BeauSoleil")
+                .font(.title)
+                .fontWeight(.black)
+                .foregroundColor(Color.blue)
+                .multilineTextAlignment(.center)
+                .padding()
+            Text("Capbreton")
+                .font(.subheadline)
+                .fontWeight(.black)
+                .foregroundColor(Color.blue)
+                .multilineTextAlignment(.center)
+                .padding()
+            Image("passageSoleil")
+                .resizable()
+                .scaledToFit()
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
