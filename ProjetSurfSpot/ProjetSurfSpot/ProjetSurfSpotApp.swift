9cecee010ddd49e7a82b0ec91917625f3736ba78
JojoDz <72552227+JojoDz@users.noreply.github.com>
2021-05-03 11:25:13 +0200

    gitattributes added

---
+++ ProjetSurfSpot/ProjetSurfSpot/ProjetSurfSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetSurfSpotApp.swift
+//  ProjetSurfSpot
+//
+//  Created by Joanna on 03/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct ProjetSurfSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
